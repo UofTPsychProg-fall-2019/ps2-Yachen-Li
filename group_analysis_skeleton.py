baea24eda12bd38835cafc1ada3cb43391fc3ce0
Yachen-Li <48690119+Yachen-Li@users.noreply.github.com>
2019-10-10 15:43:39 -0400

    final submission
    final submission

--- group_analysis_skeleton.py
+++ group_analysis_skeleton.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+##!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 scene-cat problem set for PSY 1210 - Fall 2018
@@ -7,6 +14,7 @@ scene-cat problem set for PSY 1210 - Fall 2018
 """
 
 #%% import block 
+
 import numpy as np
 import scipy as sp
 import scipy.stats
@@ -20,7 +28,9 @@ import shutil
 #
 testingrooms = ['A','B','C']
 for room in testingrooms:
-...
+   file = "/Users/yachen/Documents/GitHub/ps2-Yachen-Li/" + "testingroom" + room + "/experiment_data.csv" 
+   new_file = "/Users/yachen/Documents/GitHub/ps2-Yachen-Li/" + "rawdata/" + "experiment_data_" + room + ".csv"
+   shutil.copyfile(file, new_file)
 
 
 #%%
@@ -29,14 +39,17 @@ for room in testingrooms:
 #
 data = np.empty((0,5))
 for room in testingrooms:
-...
-
-
+   new_file = "/Users/yachen/Documents/GitHub/ps2-Yachen-Li/" + "rawdata/" + "experiment_data_" + room + ".csv"
+   tmp = sp.loadtxt(new_file, delimiter = ',')
+   data = np.vstack([data, tmp])
+data
 #%%
 # calculate overall average accuracy and average median RT
 #
-acc_avg = ...   # 91.48%
-mrt_avg = ...   # 477.3ms
+median_RT = data[:,4]
+accuracy = data[:,3]
+acc_avg = np.mean(accuracy*100) # 91.48%
+mrt_avg = np.mean(median_RT)   # 477.3ms
 
 
 #%%
@@ -44,7 +57,30 @@ mrt_avg = ...   # 477.3ms
 # if statement. (i.e., loop through the data to make a sum for each condition, 
 # then divide by the number of data points going into the sum)
 #
-...
+stimulus = data[:,1]
+subject = data[:,0]
+sumRT_word = 0
+sumAcc_word = 0
+sumRT_faces = 0
+sumAcc_faces = 0
+word_count = 0
+face_count = 0
+for x in range(len(subject)):
+    if stimulus[x] == 1:
+        word_count += 1
+        sumRT_word = sumRT_word + median_RT[x]
+        sumAcc_word = sumAcc_word + accuracy[x]
+        
+    elif stimulus[x] == 2:
+        face_count += 1
+        sumRT_faces = sumRT_faces + median_RT[x]
+        sumAcc_faces = sumAcc_faces + accuracy[x]
+        
+ave_RTword = sumRT_word/word_count
+ave_Accword = (sumAcc_word/word_count)*100 
+ave_RTface = sumRT_faces/face_count
+ave_Accface = (sumAcc_faces/face_count)*100
+    
 
 # words: 88.6%, 489.4ms   faces: 94.4%, 465.3ms
 
@@ -55,18 +91,31 @@ mrt_avg = ...   # 477.3ms
 # wp - white/pleasant, bp - black/pleasant
 # (hint: only one line of code is needed per average)
 #
-acc_wp = ...  # 94.0%
-acc_bp = ...  # 88.9%
-mrt_wp = ...  # 469.6ms
-mrt_bp = ...  # 485.1ms
+acc_wp = np.mean((data[data[:,2]==1])[:,3])*100 # 94.0%
+acc_bp = np.mean((data[data[:,2]==2])[:,3])*100  # 88.9%
+mrt_wp = np.mean((data[data[:,2]==1])[:,4]) # 469.6ms
+mrt_bp = np.mean((data[data[:,2]==2])[:,4])  # 485.1ms
 
 
 #%% 
 # calculate average median RT for each of the four conditions
 # use for loops, indexing/slicing, or both!
 # (hint: might be easier to slice data into separate words and faces datasets)
-#
-...
+## columns: subject, stimulus, pairing, accuracy, median RT
+word_data = data[data[:,1]==1]
+face_data = data[data[:,1]==2]
+
+wp_wordRT = word_data[word_data[:,2] == 1][:,4]
+bp_wordRT = word_data[word_data[:,2] == 2][:,4]
+wp_faceRT = face_data[face_data[:,2] == 1][:,4]
+bp_faceRT = face_data[face_data[:,2] == 2][:,4]
+
+
+rt_wp_words = np.mean(wp_wordRT)
+rt_bp_words = np.mean(bp_wordRT)
+rt_wp_faces = np.mean(wp_faceRT)
+rt_bp_faces = np.mean(bp_faceRT)
+
 
 # words - white/pleasant: 478.4ms
 # words - black/pleasant: 500.3ms
@@ -79,8 +128,9 @@ mrt_bp = ...  # 485.1ms
 # paired-sample t-test: scipy.stats.ttest_rel()
 #
 import scipy.stats
-...
 
+t_test_word = scipy.stats.ttest_rel(wp_wordRT,bp_wordRT)
+t_test_face = scipy.stats.ttest_rel(wp_faceRT,bp_faceRT)
 # words: t=-5.36, p=2.19e-5
 # faces: t=-2.84, p=0.0096
 
@@ -88,6 +138,12 @@ import scipy.stats
 #%%
 # print out averages and t-test results
 # (hint: use the ''.format() method to create formatted strings)
+
 #
 print('\nOVERALL: {:.2f}%, {:.1f} ms'.format(100*acc_avg,mrt_avg))
-...
+
+#Print averages for each pairing conditions:
+print('\nWords:\nMedian_RT_White: {:.1f}ms \nMedian_RT_Black: {:.1f}ms \n\nFaces:\nMedian_RT_White: {:.1f}ms \nMedian_RT_Black: {:.1f}ms'.format(rt_wp_words,rt_bp_words,rt_wp_faces,rt_bp_faces))
+
+#Print t_test results:
+print('\nWords_RT: White vs Black t-test\n {}\n\nFaces_RT: White vs Black t-test\n {}'.format(t_test_word, t_test_face))
